f407beaeacba7c1517a997cffc734ee99cae9579
Phimos <41637559+Phimos@users.noreply.github.com>
2018-08-31 16:00:15 +0800

    add2

---
+++ 2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <queue>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode * addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode * ans = new ListNode((l1->val + l2->val) % 10);
+		ListNode * p = ans;
+		int k = (l1->val + l2->val) / 10;
+		l1 = l1->next;
+		l2 = l2->next;
+		while (l1 != NULL || l2 != NULL || k != 0)
+		{
+			p->next = new ListNode(((l1 != NULL ? l1->val : 0) + (l2 != 0 ? l2->val : 0) + k) % 10);
+			k = ((l1 != NULL ? l1->val : 0) + (l2 != 0 ? l2->val : 0) + k) / 10;
+			p = p->next;
+			if (l1 != NULL)
+				l1 = l1->next;
+			if (l2 != NULL)
+				l2 = l2->next;
+		}
+		return ans;
+	}
+};
